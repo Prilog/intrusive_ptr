a0c0cf96d249f609b7d36b8d0ea8fcd2a51f235f
Prilog <sementrapeznikov@gmail.com>
2019-03-23 11:42:46 +0300

    first commit

---
+++ intrusive_ptr.h
@@ -0,0 +1,169 @@
+//
+// Created by Семен Трапезников on 23/03/2019.
+//
+
+#ifndef UNTITLED_INTRUSIVE_PTR_H
+#define UNTITLED_INTRUSIVE_PTR_H
+
+#include <functional>
+
+template <class T>
+class intrusive_ptr {
+private:
+    T* ptr;
+
+    int add_ref() {
+        if (ptr == nullptr) {
+            return 0;
+        }
+        ptr->add_ref();
+        return ptr->get_refcnt();
+    }
+
+    int release() {
+        if (ptr == nullptr) {
+            return 0;
+        }
+        unsigned int res = ptr->get_refcnt();
+        ptr->release();
+        return res;
+    }
+
+public:
+    intrusive_ptr() {
+        ptr = nullptr;
+    }
+
+    intrusive_ptr(T* arg) {
+        ptr = arg;
+        add_ref();
+    }
+
+    template <class U>
+    explicit intrusive_ptr(U* arg) {
+        ptr = static_cast<T*>(arg);
+        add_ref();
+    }
+
+    intrusive_ptr(const intrusive_ptr<T> &arg) {
+        ptr = arg.ptr;
+        add_ref();
+    }
+
+    template <class U>
+    explicit intrusive_ptr(const intrusive_ptr<U> &arg) {
+        ptr = static_cast<T*>(arg.ptr);
+        add_ref();
+    }
+
+    ~intrusive_ptr() {
+        release();
+    }
+
+    int get_refcnt() {
+        if (ptr == nullptr) {
+            return 0;
+        }
+        return ptr->get_refcnt();
+    }
+
+    intrusive_ptr& operator=(const intrusive_ptr &arg) {
+        release();
+        ptr = arg.ptr;
+        add_ref();
+    }
+
+    template <class U>
+    intrusive_ptr& operator=(const intrusive_ptr<U> &arg) {
+        release();
+        ptr = static_cast<T*>(arg.ptr);
+        add_ref();
+    }
+
+    intrusive_ptr &operator=(T* arg) {
+        release();
+        ptr = arg;
+        add_ref();
+    }
+
+    void reset() {
+        release();
+        ptr = nullptr;
+    }
+
+    void reset(T* arg) {
+        release();
+        ptr = arg;
+        add_ref();
+    }
+
+    template <typename U>
+    friend void swap(intrusive_ptr<U>& a, intrusive_ptr<U>& b) noexcept;
+
+    void swap(intrusive_ptr& arg) {
+        swap(ptr, arg.ptr);
+    }
+
+    T& operator*() const {
+        return *ptr;
+    }
+
+    T* operator->() const {
+        return ptr;
+    }
+
+    T* get() const {
+        return ptr;
+    }
+
+    explicit operator bool() const {
+        return (ptr != nullptr);
+    }
+
+    bool operator!() {
+        return (ptr == nullptr);
+    }
+};
+
+template <class U>
+void swap(intrusive_ptr<U>& a, intrusive_ptr<U>& b) noexcept {
+    a.swap(b);
+}
+
+template<class T, class U>
+bool operator==(const intrusive_ptr<T>& a, const intrusive_ptr<U>& b) {
+    return (a.get() == b.get());
+}
+
+template<class T, class U>
+bool operator!=(const intrusive_ptr<T>& a, const intrusive_ptr<U>& b) {
+    return (a.get() != b.get());
+}
+
+template<class T, class U>
+bool operator==(const intrusive_ptr<T>& a, U* b) {
+    return (a.get() == b);
+}
+
+template<class T, class U>
+bool operator!=(const intrusive_ptr<T>& a, U* b) {
+    return (a.get() != b);
+}
+
+template<class T, class U>
+bool operator==(T* a, const intrusive_ptr<U>& b) {
+    return (a == b.get());
+}
+
+template<class T, class U>
+bool operator!=(T* a, const intrusive_ptr<U>& b) {
+    return (a != b.get());
+}
+
+template <class T, class U>
+intrusive_ptr<T> dynamic_pointer_cast(const intrusive_ptr<U>& arg) {
+    return intrusive_ptr<T>(dynamic_cast<T*>(arg.get()));
+}
+
+
+#endif //UNTITLED_INTRUSIVE_PTR_H
---
+++ main.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "intrusive_ptr.h"
+
+using namespace std;
+
+struct A{
+    string arg;
+    int cnt;
+
+    A() {
+        arg = "KRATOS";
+        cnt = 0;
+    }
+
+    A(string a) {
+        arg = a;
+        cnt = 0;
+    }
+
+    void add_ref() {
+        cnt++;
+    }
+
+    void release() {
+        cnt--;
+    }
+    int get_refcnt() {
+        return cnt;
+    }
+};
+
+struct B : A {
+    B() {
+        arg = "BOI";
+    }
+};
+
+bool base_test() {
+    bool res = true;
+
+    A* pupa = new A("PUPA");
+    A* lupa = new A("LUPA");
+
+    intrusive_ptr<A> ptr1(pupa);
+    intrusive_ptr<A> ptr2(lupa);
+
+    res &= (pupa->get_refcnt() == 1);
+    res &= (lupa->get_refcnt() == 1);
+
+    res &= (ptr1 != ptr2);
+
+    ptr1.~intrusive_ptr();
+    ptr2.~intrusive_ptr();
+
+    res &= (pupa->get_refcnt() == 0);
+    res &= (lupa->get_refcnt() == 0);
+
+    return res;
+}
+
+bool comparison_test() {
+    bool res = true;
+
+    A* pupa = new A("PUPA");
+    A* lupa = new A("LUPA");
+
+    intrusive_ptr<A> ptr1(pupa);
+    intrusive_ptr<A> ptr2(lupa);
+    intrusive_ptr<A> ptr3(pupa);
+
+    res &= (ptr1 == ptr3);
+    res &= (ptr1 != ptr2);
+    res &= (ptr2 != ptr3);
+    res &= (ptr1 == pupa);
+    res &= (ptr2 == lupa);
+    res &= (pupa == ptr1);
+    res &= (lupa == ptr2);
+
+    return res;
+}
+
+bool a_lot_of_objects_test() {
+    bool res = true;
+    A* leha = new A("LEHA");
+    vector <intrusive_ptr<A>> v2;
+    for (int i = 0; i < 100; i++) {
+        v2.push_back(intrusive_ptr<A>(leha));
+        res &= (leha->get_refcnt() == (i + 1));
+    }
+
+    for (int i = 99; i >= 0; i--) {
+        v2[i].~intrusive_ptr();
+        res &= (leha->get_refcnt() == i);
+    }
+    return res;
+}
+
+bool reset_test() {
+    bool res = true;
+
+    A* pupa = new A("PUPA");
+    A* lupa = new A("LUPA");
+
+    intrusive_ptr<A> ptr1(pupa);
+    intrusive_ptr<A> ptr2(lupa);
+
+    res &= (ptr1 != ptr2);
+
+    ptr1.reset(lupa);
+
+    res &= (ptr1 == ptr2);
+
+    ptr1.reset();
+    ptr2.reset();
+
+    res &= (ptr1 == ptr2);
+    res &= (ptr1.get() == nullptr);
+
+    res &= (pupa->get_refcnt() == 0);
+    res &= (lupa->get_refcnt() == 0);
+
+    return res;
+}
+
+bool pointer_test() {
+    bool res = true;
+
+    A* pupa = new A("PUPA");
+    A* lupa = new A("LUPA");
+
+    intrusive_ptr<A> ptr1(pupa);
+    intrusive_ptr<A> ptr2(lupa);
+
+    res &= (ptr1.get() == pupa);
+    res &= (ptr2.get() == lupa);
+
+    res &= (ptr1->arg == "PUPA");
+    res &= (ptr2->arg == "LUPA");
+
+    res &= ((*ptr1).arg == "PUPA");
+    res &= ((*ptr2).arg == "LUPA");
+
+    return res;
+}
+
+bool bool_test() {
+    bool res = true;
+
+    A* pupa = new A("PUPA");
+    A* lupa = new A("LUPA");
+
+    intrusive_ptr<A> ptr1(pupa);
+    intrusive_ptr<A> ptr2(lupa);
+
+    res &= bool(ptr1);
+    res &= bool(ptr2);
+
+    res &= !(intrusive_ptr<A>());
+
+    ptr1.reset();
+
+    res &= (!ptr1);
+
+    return res;
+}
+
+/*
+bool swap_test() {
+    bool res = true;
+
+    A* pupa = new A("PUPA");
+    A* lupa = new A("LUPA");
+
+    intrusive_ptr<A> ptr1(pupa);
+    intrusive_ptr<A> ptr2(lupa);
+
+    swap(ptr1, ptr2);
+
+    res &= (pupa->get_refcnt() == 1);
+    res &= (lupa->get_refcnt() == 1);
+
+    res &= (ptr1->arg == "LUPA");
+    res &= (ptr2->arg == "PUPA");
+
+    return res;
+}*/
+
+void run_test(bool f(), string name) {
+    cout << "RUNNING: " << name << endl;
+    try {
+        if (f()) {
+            cout << "DONE" << endl;
+        } else {
+            cout << "FAIL" << endl;
+        }
+    } catch(exception& e) {
+        cout << e.what() << endl << "EXCEPTION FOUND" << endl;
+    }
+}
+
+int main() {
+    run_test(&base_test, "base_test");
+    run_test(&comparison_test, "cmp_test");
+    run_test(&a_lot_of_objects_test, "multi_ptr_test");
+    run_test(&reset_test, "reset_test");
+    run_test(&pointer_test, "pointer_test");
+    run_test(&bool_test, "bool_test");
+    //run_test(&swap_test, "swap_test");
+    return 0;
+}
